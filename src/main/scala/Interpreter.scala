d64688818ea3270e1ba78c8bcd7a77a3e281584c
Yuvi Masory <ymasory@gmail.com>
2010-12-02 02:09:46 -0500

    hook into interpreter

---
+++ src/main/scala/Interpreter.scala
@@ -0,0 +1,24 @@
+package com.yuvimasory.toylisp
+
+import scala.collection.{mutable => m}
+
+/** A stateful interpreter.
+  * Later invocations of `interpret` are affected by earlier ones.
+  */
+class Interpreter {
+
+  val environment = m.Map.empty[ToySymbol, ToyForm]
+
+  def interpret(form: ToyForm): ToyForm = {
+    form match {
+      case ToyChar(_) | ToyNumber(_) => form
+      case symb: ToySymbol => environment(symb)
+      case lst: ToyList => functionApplication(lst)
+      case lst: ToyQList => makeList(lst)
+    }
+  }
+
+  def functionApplication(lst: ToyList): ToyForm = lst
+
+  def makeList(lst: ToyQList): ToyForm = lst
+}
