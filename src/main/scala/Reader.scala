80b63ff8c538a0a0d0edf822d009d0d086501208
Yuvi Masory <ymasory@gmail.com>
2010-12-02 00:01:55 -0500

    remove list parsers, approach cannot parse s-expressions

--- src/main/scala/Reader.scala
+++ src/main/scala/Reader.scala
@@ -7,7 +7,7 @@ object Reader {
 
   def read(programText: String) = {
     import Parser._
-    parseAll(program, programText) match {
+    parseAll(toyExpression, programText) match {
       case Success(ast, _) => Some(ast)
       case _ => None
     }
@@ -15,7 +15,7 @@ object Reader {
 
   private[toylisp] object Parser extends RegexParsers with JavaTokenParsers {
 
-    lazy val program: Parser[List[ToyList]] = ((ws*) ~> toyList <~ (ws*))*
+    lazy val program: Parser[List[ToyExpression]] = (((ws*) ~> toyExpression <~ (ws*))*)
 
     //we will handle whitepsace ourselves
     override val skipWhitespace = false
@@ -23,25 +23,15 @@ object Reader {
     lazy val lParen: Parser[String] = "("
     lazy val rParen: Parser[String] = ")"
     lazy val quote : Parser[String] = "'"
-    lazy val dQuote: Parser[String] = "\""
     lazy val ws    : Parser[String] = """\s+""".r
 
     lazy val toyToken : Parser[ToyToken]  = """[a-zA-Z_@~%!=#\-\+\*\?\^\&]+""".r ^^ {ToyToken(_)}
-    lazy val toyString: Parser[ToyString] = dQuote ~> "[^\"]*".r <~ dQuote ^^ {ToyString(_)}
+    lazy val toyString: Parser[ToyString] = stringLiteral ^^ {ToyString(_)}
     lazy val toyNumber: Parser[ToyNumber] = floatingPointNumber ^^ {
       x => ToyNumber(x.toDouble)
     }
 
     lazy val toyExpression: Parser[ToyExpression] = toyToken | toyNumber | toyString
-
-    lazy val toyFunctionList: Parser[ToyFunctionList] = lParen ~> (ws*) ~> toyToken ~ (((ws+) ~> toyExpression <~ 
-      (ws*))*) <~ rParen ^^ {
-        case token ~ exprs => ToyFunctionList(token :: exprs)
-      }
-    lazy val toyExpressionList: Parser[ToyExpressionList] = quote ~> toyFunctionList ^^ {
-        case ToyFunctionList(lst) => ToyExpressionList(lst)
-    }
-    lazy val toyList: Parser[ToyList] = toyFunctionList | toyExpressionList
   }
 }
 
@@ -49,8 +39,3 @@ sealed abstract class ToyExpression
 case class ToyString(str: String) extends ToyExpression
 case class ToyToken(str: String) extends ToyExpression
 case class ToyNumber(dub: Double) extends ToyExpression
-
-sealed abstract class ToyList(lst: List[ToyExpression])
-case class ToyExpressionList(lst: List[ToyExpression]) extends ToyList(lst)
-case class ToyFunctionList(lst: List[ToyExpression]) extends ToyList(lst)
-
