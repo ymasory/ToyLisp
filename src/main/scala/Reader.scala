dbac08d6161387880276011b4878d79500db8a6c
Yuvi Masory <ymasory@gmail.com>
2010-12-02 01:14:52 -0500

    finish handling of strings

--- src/main/scala/Reader.scala
+++ src/main/scala/Reader.scala
@@ -7,7 +7,7 @@ object Reader {
 
   def read(programText: String) = {
     import Parser._
-    parseAll(toyForm, programText) match {
+    parseAll(program, programText) match {
       case Success(ast, _) => Some(ast)
       case _ => None
     }
@@ -15,34 +15,52 @@ object Reader {
 
   private[toylisp] object Parser extends RegexParsers with JavaTokenParsers {
 
-    lazy val program: Parser[List[ToyForm]] = (((ws*) ~> toyForm <~ (ws*))*)
+    //in the tradition of Lisp, a program is a List of Lists
+    lazy val program: Parser[ToyList] = (((ws*) ~> toyList <~ (ws*))*) ^^ {ToyList(_)}
 
     //we will handle whitepsace ourselves
     override val skipWhitespace = false
 
+    lazy val quoteStr: String = "'"
+
+    //handy string/regex parsers
     lazy val lParen: Parser[String] = "("
     lazy val rParen: Parser[String] = ")"
-    lazy val quote : Parser[String] = "'"
+    lazy val quote : Parser[String] = quoteStr
     lazy val ws    : Parser[String] = """\s+""".r
 
+    //"primitive types" parsers
     lazy val toySymbol: Parser[ToySymbol] = """[a-zA-Z_@~%!=#\-\+\*\?\^\&]+""".r ^^ {ToySymbol(_)}
     lazy val toyChar  : Parser[ToyChar]   = quote ~> "[^.]".r <~ quote ^^ {s => ToyChar(s charAt 0)}
     lazy val toyNumber: Parser[ToyNumber] = floatingPointNumber ^^ {
       str => ToyNumber(str.toDouble)
     }
-    lazy val toyList  : Parser[ToyList] = lParen ~> (((ws*) ~> toyForm <~ (ws*))*) <~ rParen ^^ {
+
+    //syntactic sugar parsers
+    lazy val toyString: Parser[ToyList] = stringLiteral ^^ {
+      str => {
+        val chars = str.toList.map(quoteStr + _ + quoteStr)
+        val sExpr = "(" + chars.mkString(" ") + ")"
+        parse(toyList, sExpr).get
+      }
+    } 
+
+    //list types parser
+    lazy val toyList: Parser[ToyList] = lParen ~> (((ws*) ~> toyForm <~ (ws*))*) <~ rParen ^^ {
       forms => ToyList(forms)
     }
+    lazy val toyQList: Parser[ToyQList] = quote ~> toyList ^^ {
+      case ToyList(lists) => ToyQList(lists)
+    }
 
-    //handle this with expansion handed to s-expression parser
-    lazy val toyString: Parser[Any] = stringLiteral ^^ {_ => "STRING"} 
-
-    lazy val toyForm: Parser[ToyForm] = toySymbol | toyNumber | toyChar | toyList
+    //"primitive types", list types, and sugar types together make all the forms
+    lazy val toyForm: Parser[ToyForm] = toySymbol | toyNumber | toyChar | toyList | toyQList | toyString
   }
 }
 
 sealed abstract class ToyForm
-case class ToyChar(c: Char)            extends ToyForm
-case class ToySymbol(str: String)      extends ToyForm
-case class ToyNumber(dub: Double)      extends ToyForm
-case class ToyList(lst: List[ToyForm]) extends ToyForm
+case class ToyChar  (chr: Char)          extends ToyForm
+case class ToySymbol(str: String)        extends ToyForm
+case class ToyNumber(dub: Double)        extends ToyForm
+case class ToyList  (lst: List[ToyForm]) extends ToyForm
+case class ToyQList (lst: List[ToyForm]) extends ToyForm
