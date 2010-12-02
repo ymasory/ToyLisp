6463df030f26817a3ff09eb812c5007b751f422b
Yuvi Masory <ymasory@gmail.com>
2010-12-02 02:21:27 -0500

    print results nicely with type

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,36 +11,36 @@ object Main {
   val Version = "0.1"
 
   val in = new ConsoleReader(System.in, new OutputStreamWriter(System.out))
-  
+  val interpreter = new Interpreter()
+
   def main(args: Array[String]) {
     if(args.length > 0) runFile(args(0))
     else runInteractive()
   }
 
   def runFile(path: String) {
-      val programText = Source.fromFile(path).mkString
-      Reader.read(programText) match {
-        case Some(form) => println(new Interpreter().interpret(form))
-        case None => println("syntax error")
-      }
+    val programText = Source.fromFile(path).mkString
+    giveOutput(programText)
   }
 
   def runInteractive() {
-    val interpreter = new Interpreter()
     println("\nWelcome to Toy Lisp v" + Version + "! Press Ctrl+D to exit.\n")
     while(true) {
       in.readLine(">> ") match {
-        case input: String => {
-          Reader.read(input) match {
-            case Some(form) => println(interpreter.interpret(form))
-            case None => println("syntax error")
-          }
-        }
-        case _ => {
-          println("okbye!")
-          return
-        }
+        case input: String => giveOutput(input)
+        case _ => return println("okbye!")
       }
+    }
+  }
+
+  private def giveOutput(programText: String) {
+    Reader.read(programText) match {
+      case Some(form) => {
+        val result: ToyForm = interpreter.interpret(form)
+        val resType = result.getClass.toString.split("\\s+")(1).split("\\.").last
+        println(resType + " = " + result)
+      }
+      case None => println("syntax error")
     }
   }
 }
