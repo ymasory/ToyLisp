0baf87fd9ad1a0d6f0df4a7083f164f660dcf500
Yuvi Masory <ymasory@gmail.com>
2010-12-01 22:48:11 -0500

    fix regex escape characters

--- src/main/scala/Reader.scala
+++ src/main/scala/Reader.scala
@@ -5,7 +5,7 @@ import scala.util.parsing.combinator._
 object Reader {
 
 
-  def read(programText: String): Option[List[ToyList]] = {
+  def read(programText: String) = {
     import Parser._
     parseAll(program, programText) match {
       case Success(ast, _) => Some(ast)
@@ -20,25 +20,25 @@ object Reader {
     //we will handle whitepsace ourselves
     override val skipWhitespace = false
 
-    private lazy val lParen: Parser[String] = "("
-    private lazy val rParen: Parser[String] = ")"
-    private lazy val quote : Parser[String] = "'"
-    private lazy val ws    : Parser[String] = """\s+""".r
+    lazy val lParen: Parser[String] = "("
+    lazy val rParen: Parser[String] = ")"
+    lazy val quote : Parser[String] = "'"
+    lazy val ws    : Parser[String] = """\s+""".r
 
-    private lazy val toyToken : Parser[ToyToken]  = "[a-zA-Z_+-*/%!=&^]+".r ^^ {ToyToken(_)}
-    private lazy val toyString: Parser[ToyString] = "[^\"]*?".r             ^^ {ToyString(_)}
-    private lazy val toyNumber: Parser[ToyNumber] = floatingPointNumber ^^ {
+    lazy val toyToken : Parser[ToyToken]  = """[a-zA-Z_@~%!=#\-\+\*\?\^\&]+""".r ^^ {ToyToken(_)}
+    lazy val toyString: Parser[ToyString] = "\"[^\"]*?\"".r ^^ {ToyString(_)}
+    lazy val toyNumber: Parser[ToyNumber] = floatingPointNumber ^^ {
       x => ToyNumber(x.toDouble)
     }
 
-    private lazy val toyExpression: Parser[ToyExpression] = toyToken | toyNumber | toyString
+    lazy val toyExpression: Parser[ToyExpression] = toyToken | toyNumber | toyString
 
-    private lazy val toyFunctionList: Parser[ToyFunctionList] = lParen ~> (ws*) ~> (toyExpression*) <~
+    lazy val toyFunctionList: Parser[ToyFunctionList] = lParen ~> (ws*) ~> (toyExpression*) <~
       (ws*) <~ rParen ^^ {ToyFunctionList(_)}
-    private lazy val toyExpressionList: Parser[ToyExpressionList] = quote ~> toyFunctionList ^^ {
+    lazy val toyExpressionList: Parser[ToyExpressionList] = quote ~> toyFunctionList ^^ {
         case ToyFunctionList(lst) => ToyExpressionList(lst)
     }
-    private lazy val toyList: Parser[ToyList] = toyFunctionList | toyExpressionList
+    lazy val toyList: Parser[ToyList] = toyFunctionList | toyExpressionList
   }
 }
 
