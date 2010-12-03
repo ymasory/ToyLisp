946c60fc93a9f94c748e9d3649fbdffd1e5a1b0d
Yuvi Masory <ymasory@gmail.com>
2010-12-03 02:00:58 -0500

    rename isFalsy to falsy

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -23,7 +23,7 @@ class Interpreter {
   private val zero = ToyNumber(0.0)
   private val emptyList = ToyList(Nil)
 
-  private def isFalsy(form: ToyForm): Boolean = {
+  private def falsy(form: ToyForm): Boolean = {
     form match {
       case ToyCall(Nil) => true
       case `zero` => true
@@ -38,81 +38,87 @@ class Interpreter {
     interpret(form)
   }
 
+  private def handleLambda(lambda: ToyLambda, forms: List[ToyForm]) = {
+    lambda match {
+      case ToyLambda(args, body) => {
+        if (args.length == forms.length) {
+          for (i <- (0 until args.length)) {
+            environment.update(args(i), interpret(forms(i)))
+          }
+          interpret(body)
+        }
+        else throw SyntaxError("tried to call a lambda with wrong number of args")
+      }
+    }
+  }
+
   private def functionApplication(toyCall: ToyCall): ToyForm = {
     toyCall match {
-      case ToyCall(h :: t) => h match {
-        case ToyLambda(args, body) => {
-          if (args.length == t.length) {
-            for (i <- (0 until args.length)) {
-              environment.update(args(i), interpret(t(i)))
-            }
-            interpret(body)
-          } 
-          else throw SyntaxError("tried to call a lambda with wrong number of args")
-        }
-        case ToySymbol("set!") => t match {
+      case ToyCall(firstForm :: restForms) => firstForm match {
+        case tl: ToyLambda => handleLambda(tl, restForms)
+        case ToySymbol("set!") => restForms match {
           case List(ToySymbol(v), form) => {
             environment.update(ToySymbol(v), interpret(form))
             emptyList
           }
           case _ => throw SyntaxError("set needs a symbol and a form")
         }
-        case ToySymbol("list?") => (t map interpret) match {
+        case ToySymbol("list?") => (restForms map interpret) match {
           case List(ToyList(_)) => one
           case _ => zero
         }
-        case ToySymbol("char?") => (t map interpret) match {
+        case ToySymbol("char?") => (restForms map interpret) match {
           case List(ToyChar(_)) => one
           case _ => zero
         }
-        case ToySymbol("num?") => (t map interpret) match {
+        case ToySymbol("num?") => (restForms map interpret) match {
           case List(ToyNumber(_)) => one
           case _ => zero
         }
-        case ToySymbol("eq?") => (t map interpret) match {
+        case ToySymbol("eq?") => (restForms map interpret) match {
           case List(ToyNumber(a), ToyNumber(b)) => if (a == b) one else zero
           case List(ToyChar(a), ToyChar(b)) => if (a == b) one else zero
           case List(ToyList(a), ToyList(b)) => if (a == b) one else zero
           case _ => zero
         }
-        case ToySymbol("char>num") => (t map interpret) match {
+        case ToySymbol("char>num") => (restForms map interpret) match {
           case List(ToyChar(c)) => ToyNumber(c.toInt)
           case _ => throw SyntaxError("char>num needs one char")
         }
-        case ToySymbol("num>char") => (t map interpret) match {
+        case ToySymbol("num>char") => (restForms map interpret) match {
           case List(ToyNumber(n)) => ToyChar(n.toChar)
           case _ => throw SyntaxError("num>char needs one number")
         }
-        case ToySymbol("+") => (t map interpret) match {
+        case ToySymbol("+") => (restForms map interpret) match {
           case List(ToyNumber(a), ToyNumber(b)) => ToyNumber(a + b)
           case _ => throw SyntaxError("plus needs two numbers")
         }
-        case ToySymbol("opp") => (t map interpret) match {
+        case ToySymbol("opp") => (restForms map interpret) match {
           case List(ToyNumber(a)) => ToyNumber(-a)
           case _ => throw SyntaxError("opp needs one number")
         }
-        case ToySymbol("<=") => (t map interpret) match {
+        case ToySymbol("<=") => (restForms map interpret) match {
           case List(ToyNumber(a), ToyNumber(b)) => if (a <= b) one else zero
           case _ => throw SyntaxError("plus needs two numbers")
         }
-        case ToySymbol("floor") => (t map interpret) match {
+        case ToySymbol("floor") => (restForms map interpret) match {
           case List(ToyNumber(a)) => ToyNumber(java.lang.Math.floor(a))
           case _ => throw SyntaxError("floor requires one argument")
         }
-        case ToySymbol("cons") => (t map interpret) match {
+        case ToySymbol("cons") => (restForms map interpret) match {
           case List(a, ToyList(q)) => ToyList(interpret(a) :: q)
           case _ => throw SyntaxError("cons needs a form and a list")
         }
-        case ToySymbol("head") => (t map interpret) match {
+        case ToySymbol("head") => (restForms map interpret) match {
           case List(ToyList(h :: t)) => h
           case _ => throw SyntaxError("head needs a non-empty quoted list")
         }
-        case ToySymbol("tail") => (t map interpret) match {
+        case ToySymbol("tail") => (restForms map interpret) match {
           case List(ToyList(h :: t)) => ToyList(t)
           case _ => throw SyntaxError("tail needs a non-empty quoted list")
         }
-        case ToySymbol("if") => t match {
-          case List(cond, ift, iff) => interpret(  if (isFalsy(interpret(cond))) ift else iff  )
+        case ToySymbol("if") => restForms match {
+          case List(cond, ift, iff) => interpret(  if (falsy(interpret(cond))) ift else iff  )
           case _ => throw SyntaxError("if requires three arguments")
         }
         case userFunc => {
